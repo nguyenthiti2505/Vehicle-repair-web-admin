8f2754f3b85da3b718493e4426c0c91121039473
nguyenthiti2505 <ti.nguyen@student.passerellesnumeriques.org>
2020-06-15 18:59:38 +0700

    sourcode to code

---
+++ src/containers/_nav.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: 'cil-speedometer',
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    }
+  },
+  {
+    _tag: 'CSidebarNavTitle',
+    _children: ['Theme']
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Colors',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Typography',
+    to: '/theme/typography',
+    icon: 'cil-pencil',
+  },
+]
+
