2891236724396cada9bf4aa3d19157942871be38
dinhthithanh99 <thahthanh968@gmail.com>
2020-07-25 11:48:16 +0700

    dashboard function

--- src/views/widgets/WidgetsDropdown.js
+++ src/views/widgets/WidgetsDropdown.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
+
 import {
   CWidgetDropdown,
   CRow,
@@ -6,26 +9,64 @@ import {
   CDropdown,
   CDropdownMenu,
   CDropdownItem,
-  CDropdownToggle
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import ChartLineSimple from '../charts/ChartLineSimple'
-import ChartBarSimple from '../charts/ChartBarSimple'
+  CDropdownToggle,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import ChartLineSimple from "../charts/ChartLineSimple";
+import ChartBarSimple from "../charts/ChartBarSimple";
+
+import { fetchUsers } from "../../redux/userRedux/actions";
+import { fetchStations } from "../../redux/stationRedux/actions";
+import { callApi } from "../../utils/apiCaller";
 
 const WidgetsDropdown = () => {
-  // render
+  const dispatch = useDispatch();
+
+  const history = useHistory();
+
+  const { fetching, data } = useSelector((state) => state.user);
+  const dataStation = useSelector((state) => state.station.data);
+
+  const [pageSize, setPageSize] = useState(10);
+  const [orders, setOrders] = useState([]);
+
+  const queryPage = useLocation().search.match(/page=([0-9]+)/, "");
+
+  const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1);
+
+  const [page, setPage] = useState(currentPage);
+
+  const fetchStationOrders = async () => {
+    try {
+      const token = localStorage.getItem("_token");
+      const response = await callApi(`dashboard/orders`, "GET", null, token);
+      setOrders(response.data);
+    } catch (e) {
+      console.log("fetchOrders -> e.response", e.response);
+    }
+  };
+
+  useEffect(() => {
+    currentPage !== page && setPage(currentPage);
+    dispatch(fetchUsers({ pageIndex: page, pageSize }));
+    currentPage !== page && setPage(currentPage);
+    dispatch(fetchStations({ pageIndex: page, pageSize }));
+    fetchStationOrders();
+  }, [currentPage, pageSize, page, dispatch, history]);
+  console.log(dataStation);
+
   return (
     <CRow>
-      <CCol sm="6" lg="3">
+      <CCol sm="6" lg="3" spinning={fetching}>
         <CWidgetDropdown
           color="gradient-primary"
-          header="9.823"
-          text="Members online"
+          header={data?.totalCount}
+          text="Users"
           footerSlot={
             <ChartLineSimple
               pointed
               className="c-chart-wrapper mt-3 mx-3"
-              style={{height: '70px'}}
+              style={{ height: "70px" }}
               dataPoints={[65, 59, 84, 84, 51, 55, 40]}
               pointHoverBackgroundColor="primary"
               label="Members"
@@ -35,7 +76,7 @@ const WidgetsDropdown = () => {
         >
           <CDropdown>
             <CDropdownToggle color="transparent">
-              <CIcon name="cil-settings"/>
+              <CIcon name="cil-settings" />
             </CDropdownToggle>
             <CDropdownMenu className="pt-0" placement="bottom-end">
               <CDropdownItem>Action</CDropdownItem>
@@ -50,16 +91,16 @@ const WidgetsDropdown = () => {
       <CCol sm="6" lg="3">
         <CWidgetDropdown
           color="gradient-info"
-          header="9.823"
-          text="Members online"
+          header={dataStation?.totalCount}
+          text="Stations"
           footerSlot={
             <ChartLineSimple
               pointed
               className="mt-3 mx-3"
-              style={{height: '70px'}}
+              style={{ height: "70px" }}
               dataPoints={[1, 18, 9, 17, 34, 22, 11]}
               pointHoverBackgroundColor="info"
-              options={{ elements: { line: { tension: 0.00001 }}}}
+              options={{ elements: { line: { tension: 0.00001 } } }}
               label="Members"
               labels="months"
             />
@@ -67,7 +108,7 @@ const WidgetsDropdown = () => {
         >
           <CDropdown>
             <CDropdownToggle caret={false} color="transparent">
-              <CIcon name="cil-location-pin"/>
+              <CIcon name="cil-location-pin" />
             </CDropdownToggle>
             <CDropdownMenu className="pt-0" placement="bottom-end">
               <CDropdownItem>Action</CDropdownItem>
@@ -82,15 +123,15 @@ const WidgetsDropdown = () => {
       <CCol sm="6" lg="3">
         <CWidgetDropdown
           color="gradient-warning"
-          header="9.823"
-          text="Members online"
+          header={orders?.totalCount}
+          text="Orders"
           footerSlot={
             <ChartLineSimple
               className="mt-3"
-              style={{height: '70px'}}
+              style={{ height: "70px" }}
               backgroundColor="rgba(255,255,255,.2)"
               dataPoints={[78, 81, 80, 45, 34, 12, 40]}
-              options={{ elements: { line: { borderWidth: 2.5 }}}}
+              options={{ elements: { line: { borderWidth: 2.5 } } }}
               pointHoverBackgroundColor="warning"
               label="Members"
               labels="months"
@@ -99,7 +140,7 @@ const WidgetsDropdown = () => {
         >
           <CDropdown>
             <CDropdownToggle color="transparent">
-              <CIcon name="cil-settings"/>
+              <CIcon name="cil-settings" />
             </CDropdownToggle>
             <CDropdownMenu className="pt-0" placement="bottom-end">
               <CDropdownItem>Action</CDropdownItem>
@@ -119,7 +160,7 @@ const WidgetsDropdown = () => {
           footerSlot={
             <ChartBarSimple
               className="mt-3 mx-3"
-              style={{height: '70px'}}
+              style={{ height: "70px" }}
               backgroundColor="rgb(250, 152, 152)"
               label="Members"
               labels="months"
@@ -128,7 +169,7 @@ const WidgetsDropdown = () => {
         >
           <CDropdown>
             <CDropdownToggle caret className="text-white" color="transparent">
-              <CIcon name="cil-settings"/>
+              <CIcon name="cil-settings" />
             </CDropdownToggle>
             <CDropdownMenu className="pt-0" placement="bottom-end">
               <CDropdownItem>Action</CDropdownItem>
@@ -140,7 +181,7 @@ const WidgetsDropdown = () => {
         </CWidgetDropdown>
       </CCol>
     </CRow>
-  )
-}
+  );
+};
 
-export default WidgetsDropdown
+export default WidgetsDropdown;
