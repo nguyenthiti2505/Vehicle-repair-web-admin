2c0a81bcf4e35e99b829436e17b7c003f68d0c25
dinhthithanh99 <thahthanh968@gmail.com>
2020-08-08 16:09:51 +0700

    banned and active user

--- src/views/users/User.js
+++ src/views/users/User.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import { CCard, CCardBody, CCardHeader, CCol, CRow } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
@@ -13,21 +12,14 @@ const User = ({ match }) => {
   const user = useSelector((state) =>
     state.user.data.sources.find((u) => u.id === match.params.id)
   );
-  const userDetails = user
-    ? Object.entries(user)
-    : [
-        [
-          "id",
-          <span>
-            <CIcon className="text-muted" name="cui-icon-ban" />
-            Not found
-          </span>,
-        ],
-      ];
-  console.log(`hihihihii${userDetails}`);
-  const handleChangeBannedUser = async (value) => {
+
+  useEffect(() => {
+    setUser(user);
+  })
+
+  const handleChangeBannedUser = async () => {
     try {
-      const isActive = value ? "true" : "false";
+      const isActive = (user?.isActive) ? false : true;
       const token = localStorage.getItem("_token");
       const response = await callApi(
         `account/${id}`,
@@ -35,7 +27,6 @@ const User = ({ match }) => {
         { isActive },
         token
       );
-      console.log(`1111${response}`);
       setUser(response.data);
     } catch (e) {
       console.log("banned user -> e.response", e.response);
@@ -50,35 +41,76 @@ const User = ({ match }) => {
             <CRow>
               <CCol lg="9">User id: {match.params.id}</CCol>
               <CCol lg="3">
-                {userDetails.map(([key, value]) => {
-                  if (key == "isActive") {
-                    return (
-                      <button onClick={handleChangeBannedUser(value)}>
-                        {" "}
-                        {key ? "Active" : "Banned"}
-                      </button>
-                    );
-                  }
-                })}
+                  {(userData?.isActive) ? (
+                  <button 
+                    onClick={handleChangeBannedUser}
+                  >
+                      Active
+                  </button>
+                  ) : (
+                  <button 
+                    onClick={handleChangeBannedUser}
+                  >
+                      Banned
+                  </button>
+                  )}
+                
               </CCol>
             </CRow>
           </CCardHeader>
           <CCardBody>
+            {userData ? (
             <table className="table table-striped table-hover">
               <tbody>
-                {userDetails.map(([key, value], index) => {
-                  console.log(`key-${key}: value-${value}`);
-                  return (
-                    <tr key={index.toString()}>
-                      <td>{`${key}:`}</td>
-                      <td>
-                        <strong>{value ? value.toString() : value}</strong>
-                      </td>
-                    </tr>
-                  );
-                })}
+                <tr>
+                  <td>ID: </td>
+                  <td>{userData.id}</td>
+                </tr>
+                <tr>
+                  <td>Name: </td>
+                  <td>{userData.name}</td>
+                </tr>
+                <tr>
+                  <td>Roles: </td>
+                  <td>{userData.roles}</td>
+                </tr>
+                <tr>
+                  <td>Email: </td>
+                  <td>{userData.email}</td>
+                </tr>
+                <tr>
+                  <td>Email Confirmed: </td>
+                  <td>{userData.emailConfirmed ? 'true' : 'false'}</td>
+                </tr>
+                <tr>
+                  <td>Phone Number: </td>
+                  <td>{userData.phoneNumber}</td>
+                </tr>
+                <tr>
+                  <td>Phone Number Confirmed: </td>
+                  <td>{userData.phoneNumberConfirmed ? 'true' : 'false'}</td>
+                </tr>
+                <tr>
+                  <td>Address: </td>
+                  <td>{userData.address}</td>
+                </tr>
+                <tr>
+                  <td>Device Token: </td>
+                  <td>{userData.deviceToken}</td>
+                </tr>
+                <tr>
+                  <td>Created On: </td>
+                  <td>{userData.createdOn}</td>
+                </tr>
               </tbody>
             </table>
+            ) : (
+              <span>
+            <CIcon className="text-muted" name="cui-icon-ban" />
+            Not found
+          </span>
+            )
+}
           </CCardBody>
         </CCard>
       </CCol>
